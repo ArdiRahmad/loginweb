c408ead8446e5c747bd1fc737c52cf50d53bfef0
Ardi Rahmad H <70589974+ArdiRahmad@users.noreply.github.com>
2020-10-10 14:24:28 +0700

    Add files via upload

---
+++ query.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE 'users' (
+  'userName' varchar(100) NOT NULL,
+  'userPassword' varchar(100) NOT NULL,
+  'admintoken' varchar(100) NOT NULL,
+  PRIMARY KEY ('userName')  
+)
+
+INSERT INTO `buku` (`userName`, `userPassword`, `admintoken`) VALUES
+('oda', '1234', '111111');
